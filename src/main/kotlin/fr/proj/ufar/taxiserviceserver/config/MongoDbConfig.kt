025534b9b9889aa3643f711dd44b8303d52d87c5
0xAB <abgar.abrahamyan@picsart.com>
2022-12-11 19:18:55 +0400

    Moved MongoDbConfig file into the config package.

--- src/main/kotlin/fr/proj/ufar/taxiserviceserver/config/MongoDbConfig.kt
+++ src/main/kotlin/fr/proj/ufar/taxiserviceserver/config/MongoDbConfig.kt
@@ -1,4 +1,4 @@
-package fr.proj.ufar.taxiserviceserver
+package fr.proj.ufar.taxiserviceserver.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
